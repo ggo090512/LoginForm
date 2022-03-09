7a056bb092ead1870cba85d078f4861c567b2f9d
MinhHieu <hieumd090500@gmail.com>
2022-03-09 09:24:13 +0700

    commit lbMakhau

--- Login/Login/Form1.Designer.cs
+++ Login/Login/Form1.Designer.cs
@@ -31,33 +31,62 @@ namespace Login
         {
             this.txtUsername = new System.Windows.Forms.TextBox();
             this.lbUser = new System.Windows.Forms.Label();
+            this.lbMatkau = new System.Windows.Forms.Label();
+            this.txtMatkhau = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // txtUsername
             // 
             this.txtUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUsername.Location = new System.Drawing.Point(246, 85);
+            this.txtUsername.Location = new System.Drawing.Point(328, 105);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(4);
             this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(228, 29);
+            this.txtUsername.Size = new System.Drawing.Size(303, 34);
             this.txtUsername.TabIndex = 0;
             // 
             // lbUser
             // 
             this.lbUser.AutoSize = true;
             this.lbUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbUser.Location = new System.Drawing.Point(130, 90);
+            this.lbUser.Location = new System.Drawing.Point(173, 111);
+            this.lbUser.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbUser.Name = "lbUser";
-            this.lbUser.Size = new System.Drawing.Size(100, 24);
+            this.lbUser.Size = new System.Drawing.Size(129, 29);
             this.lbUser.TabIndex = 1;
             this.lbUser.Text = "UserName";
             // 
+            // lbMatkau
+            // 
+            this.lbMatkau.AutoSize = true;
+            this.lbMatkau.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMatkau.Location = new System.Drawing.Point(173, 214);
+            this.lbMatkau.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbMatkau.Name = "lbMatkau";
+            this.lbMatkau.Size = new System.Drawing.Size(113, 29);
+            this.lbMatkau.TabIndex = 3;
+            this.lbMatkau.Text = "Mật Khẩu";
+            this.lbMatkau.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtMatkhau
+            // 
+            this.txtMatkhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMatkhau.Location = new System.Drawing.Point(328, 208);
+            this.txtMatkhau.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMatkhau.Name = "txtMatkhau";
+            this.txtMatkhau.Size = new System.Drawing.Size(303, 34);
+            this.txtMatkhau.TabIndex = 2;
+            this.txtMatkhau.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.lbMatkau);
+            this.Controls.Add(this.txtMatkhau);
             this.Controls.Add(this.lbUser);
             this.Controls.Add(this.txtUsername);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -69,6 +98,8 @@ namespace Login
 
         private System.Windows.Forms.TextBox txtUsername;
         private System.Windows.Forms.Label lbUser;
+        private System.Windows.Forms.Label lbMatkau;
+        private System.Windows.Forms.TextBox txtMatkhau;
     }
 }
 
